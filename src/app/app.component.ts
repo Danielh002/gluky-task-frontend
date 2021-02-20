c5e0b6fd2e3e6d9457d9bda872af4252452f0ed0
Daniel <danielhernandez002@hotmail.com>
2021-02-20 16:15:19 -0500

    Update to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Roles, User } from './models/user.model';
 import { AppCommonService } from './services/app-common.service';
+import { uniqueNamesGenerator, adjectives, colors, animals } from  'unique-names-generator';
+
 
 @Component({
   selector: 'app-root',
@@ -10,18 +12,29 @@ import { AppCommonService } from './services/app-common.service';
 export class AppComponent implements OnInit {
 
   title = 'gluky-task-frontend';
-  currentRole: Roles = Roles.NO_AUTH;
+  currentUser : User;
 
   constructor(public appCommonService: AppCommonService){}
 
   ngOnInit() {
+    let randomName = uniqueNamesGenerator({
+      dictionaries: [adjectives, animals, colors], // colors can be omitted here as not used
+      length: 2
+    });
+
+    this.currentUser = {
+      name: randomName,
+      email: randomName+'@gmail.com',
+      role: Roles.NO_AUTH
+    }
+
     this.appCommonService.currentUser$.subscribe((user: User) => {
-      this.currentRole = user.role
+      this.currentUser = user
     })
   }
 
   checkRole(role: string){
-    if(this.currentRole == role){
+    if(this.currentUser.role == role){
       return true;
     }
     return false;
