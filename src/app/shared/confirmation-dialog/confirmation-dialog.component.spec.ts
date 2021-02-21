7d8c672f00dfebe1431ec2836fb14d7cce20dc52
Daniel <danielhernandez002@hotmail.com>
2021-02-21 18:03:22 -0500

    Confirmation unit test fixed

--- src/app/shared/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/shared/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
@@ -8,7 +9,11 @@ describe('ConfirmationDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmationDialogComponent ]
+      declarations: [ ConfirmationDialogComponent ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} }, 
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        ]
     })
     .compileComponents();
   });
