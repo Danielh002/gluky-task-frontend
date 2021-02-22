ceee2d1933cac3dfeda87cf1e15482ef3891c6b5
Daniel <danielhernandez002@hotmail.com>
2021-02-21 21:06:16 -0500

    Updating prod server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  server: 'http://localhost:3000',
+  server: 'https://gluky-task.ue.r.appspot.com',
   googleOAuth: '114997711738-goko2uke2ire5g3q64vm9cv6tpsc9dku.apps.googleusercontent.com'
 };
