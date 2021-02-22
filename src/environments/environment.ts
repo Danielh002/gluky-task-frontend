16713a86c3dd58e7ce4175fb861455fe20b93491
Daniel <danielhernandez002@hotmail.com>
2021-02-22 01:16:18 -0500

    Update google Auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   server: 'http://localhost:3000',
-  googleOAuth: '114997711738-goko2uke2ire5g3q64vm9cv6tpsc9dku.apps.googleusercontent.com'
+  googleOAuth: '114997711738-62e40gfl8ej2qdmfepe7s0l5o2scmigo.apps.googleusercontent.com'
 };
 
 /*
